a36a5940305a063cfe4157a7f796d9085f605832
texane <texane@gmail.com>
2016-08-31 22:45:33 +0200

    esp8266, send_udp

--- src/device/esp8266/user_main.c
+++ src/device/esp8266/user_main.c
@@ -1,3 +1,10 @@
+#define LWIP_OPEN_SRC
+#include <lwipopts.h>
+#include <lwip/api.h>
+#include <lwip/pbuf.h>
+#include <lwip/udp.h>
+#include <lwip/err.h>
+
 #include <stdint.h>
 #include <ets_sys.h>
 #include <osapi.h>
@@ -18,6 +25,174 @@
 #define TRACE()
 #define PRINTF(__s, ...)
 #endif /* CONFIG_DEBUG */
+
+
+/* wiloc message encoder */
+/* implemented with 8 bits memory limited MCUs in mind */
+
+#include <stdint.h>
+#include "../../common/dns.h"
+#include "../../common/wiloc.h"
+#include "../config.h"
+
+
+#ifndef DNS_ZONE_NAME
+#error "missing DNS_ZONE_NAME (.my.zone.com)"
+#endif /* DNS_ZONE_NAME */
+
+
+typedef uint8_t small_size_t;
+#define SMALL_SIZE_MAX ((uint8_t)-1)
+#define SMALL_SIZEOF(__x) ((small_size_t)sizeof(__x))
+
+
+static void encode_coord(uint8_t* buf, const char* coord)
+{
+  static const small_size_t precision = WILOC_COORD_PRECISION;
+  uint32_t x;
+  small_size_t i;
+  uint8_t isn;
+
+  isn = 0;
+  if (*coord == '-')
+  {
+    isn = 1;
+    ++coord;
+  }
+  else if (*coord == '+')
+  {
+    ++coord;
+  }
+
+  for (; *coord == '0'; ++coord) ;
+
+  x = 0;
+  for (; *coord && (*coord != '.'); ++coord)
+    x = x * 10 + (uint32_t)(*coord - '0');
+
+  i = 0;
+  if ((coord[0] == 0) || (coord[1] == 0)) goto skip_decimals;
+  ++coord;
+
+  /* i the maximum precision */
+  for (; *coord && (i != precision); ++i, ++coord)
+    x = x * 10 + (uint32_t)(*coord - '0');
+
+ skip_decimals:
+  for (; i != precision; ++i) x *= 10;
+  if (isn) x *= -1;
+
+  buf[0] = (uint8_t)(x >> 24);
+  buf[1] = (uint8_t)(x >> 16);
+  buf[2] = (uint8_t)(x >> 8);
+  buf[3] = (uint8_t)(x >> 0);
+}
+
+
+static small_size_t encode_base64
+(
+ const uint8_t* sbuf, small_size_t slen,
+ uint8_t* dbuf, small_size_t dlen
+)
+{
+  static const uint8_t map[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789+/";
+
+  small_size_t i;
+  small_size_t x;
+
+  if ((((slen + 2) / 3) * 4) > dlen) return 0;
+
+  /* increment 3 chars at a time */
+  for (i = 0, x = 0; x < slen; x += 3) 
+  {
+    /* turn these three chars into a 24 bits number */
+    uint32_t n = ((uint32_t)sbuf[x]) << 16;
+      
+    if ((x + 1) < slen) n += ((uint32_t)sbuf[x + 1]) << 8;
+    if ((x + 2) < slen) n += (uint32_t)sbuf[x + 2];
+
+    /* split 24 bits into 4x 6 bits numbers */
+            
+    /* if 1 byte avail, its encoding is spread over 2 chars */
+    dbuf[i++] = map[(uint8_t)(n >> 18) & 63];
+    dbuf[i++] = map[(uint8_t)(n >> 12) & 63];
+
+    /* if 2 bytes avail, encoding is spread over 3 chars */
+    if ((x + 1) < slen) dbuf[i++] = map[(uint8_t)(n >> 6) & 63];
+
+    /* if 3 bytes avail, encoding is spread over 4 chars */
+    if ((x + 2) < slen) dbuf[i++] = map[(uint8_t)n & 63];
+  }  
+
+  /* pad if not a multiple of 3 */
+  x = slen % 3;
+  if (x)
+  {
+    for (; x != 3; ++x) dbuf[i++] = '=';
+  }
+
+  return i;
+}
+
+static small_size_t encode_wiloc_msg
+(uint8_t* mbuf, small_size_t msize)
+{
+  /* encode a wiloc request */
+  /* return the request size, including terminating 0 */
+
+  /* encoding process */
+  /* encode in base64 */
+  /* add label dots */
+  /* append zone */
+  /* encode_name */
+
+  static uint8_t tmp[SMALL_SIZE_MAX];
+  small_size_t i;
+  small_size_t j;
+  small_size_t k;
+
+  /* base64 encoding */
+
+  j = encode_base64(mbuf, msize, tmp, SMALL_SIZEOF(tmp));
+
+  /* add dots every 63 bytes chars */
+  /* put a dot even at 0 for dns_name_encode to work in place */
+
+  for (i = 0, k = 0; i != j; ++i, ++k)
+  {
+    if ((i % 63) == 0) mbuf[k++] = '.';
+    mbuf[k] = tmp[i];
+  }
+
+  /* append zone */
+
+  for (i = 0; DNS_ZONE_NAME[i]; ++i, ++k) mbuf[k] = DNS_ZONE_NAME[i];
+  mbuf[k++] = 0;
+
+  /* encode DNS name in place */
+
+  mbuf[0] = 0;
+
+  for (i = 1, j = 0; mbuf[i]; ++i)
+  {
+    if (mbuf[i] == '.')
+    {
+      j = i;
+      mbuf[i] = 0;
+    }
+    else
+    {
+      ++mbuf[j];
+    }
+  }
+
+  mbuf[i++] = 0;
+
+  return i;
+}
 
 
 /* scan done task */
@@ -44,12 +219,65 @@ typedef enum
   WILOC_STATE_IDLE,
   WILOC_STATE_SCAN,
   WILOC_STATE_CONNECT,
+  WILOC_STATE_SEND,
   WILOC_STATE_DISCONNECT,
   WILOC_STATE_DONE,
   WILOC_STATE_INVALID
 } wiloc_state_t;
 
 static wiloc_state_t wiloc_state = WILOC_STATE_INIT;
+static ip_addr_t wiloc_laddr;
+static ip_addr_t wiloc_gwaddr;
+
+
+static int send_udp
+(const uint8_t* data, size_t size, ip_addr_t* daddr, uint16_t dport)
+{
+  struct udp_pcb* pcb;
+  struct pbuf* buf;
+  int err = -1;
+
+  pcb = udp_new();
+  if (pcb == NULL)
+  {
+    PERROR();
+    goto on_error_0;
+  }
+
+  buf = pbuf_alloc(PBUF_TRANSPORT, (u16_t)size, PBUF_RAM);
+  if (buf == NULL)
+  {
+    PERROR();
+    goto on_error_1;
+  }
+
+  os_memcpy(buf->payload, data, size);
+
+  if (udp_sendto(pcb, buf, daddr, dport) != ERR_OK)
+  {
+    PERROR();
+    goto on_error_2;
+  }
+
+  err = 0;
+
+ on_error_2:
+  pbuf_free(buf);
+ on_error_1:
+  udp_remove(pcb);
+ on_error_0:
+  return err;
+}
+
+
+static inline uint16_t uint16_to_be(uint16_t x)
+{
+#if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+  x = (x >> 8) | (x << 8);
+#endif
+  return x;
+}
+
 
 static void wiloc_next(void* p)
 {
@@ -75,6 +303,8 @@ static void wiloc_next(void* p)
 
   case WILOC_STATE_SCAN:
     {
+      /* TODO: prepare wiloc message here */
+
       struct bss_info* bi;
       const struct bss_info* open_bi;
       struct station_config sc;
@@ -88,8 +318,8 @@ static void wiloc_next(void* p)
 	if ((bi->authmode == AUTH_OPEN) && (open_bi == NULL)) open_bi = bi;
 
 #ifdef CONFIG_DEBUG
-	if (bi->ssid_len <= 31) bi->ssid[bi->ssid_len] = 0;
-	else bi->ssid[31] = 0;
+	if (bi->ssid_len > 31) bi->ssid_len = 31;
+	bi->ssid[bi->ssid_len] = 0;
 
 	PRINTF
 	(
@@ -105,12 +335,11 @@ static void wiloc_next(void* p)
 	break ;
       }
 
-      TRACE();
-
       wifi_station_disconnect();
       wifi_station_set_auto_connect(0);
 
       os_memcpy(sc.ssid, open_bi->ssid, open_bi->ssid_len);
+      if (open_bi->ssid_len <= 31) sc.ssid[open_bi->ssid_len] = 0;
       sc.bssid_set = 1;
       os_memcpy(sc.bssid, open_bi->bssid, sizeof(sc.bssid));
 
@@ -146,8 +375,6 @@ static void wiloc_next(void* p)
     {
       uint8 status;
 
-      TRACE();
-
       status = wifi_station_get_connect_status();
       switch (status)
       {
@@ -167,7 +394,10 @@ static void wiloc_next(void* p)
 	   IP2STR(&ipi.ip), IP2STR(&ipi.gw)
 	  );
 
-	  wiloc_state = WILOC_STATE_DISCONNECT;
+	  os_memcpy(wiloc_laddr, &ipi.ip, sizeof(ipi.ip));
+	  os_memcpy(wiloc_gwaddr, &ipi.gw, sizeof(ipi.gw));
+
+	  wiloc_state = WILOC_STATE_SEND;
 
 	  break ;
 	}
@@ -190,6 +420,55 @@ static void wiloc_next(void* p)
       }
 
       break ;
+    }
+
+  case WILOC_STATE_SEND:
+    {
+      /* send the wiloc message */
+
+      uint8_t buf[SMALL_SIZE_MAX];
+      dns_header_t* dnsh;
+      dns_query_t* dnsq;
+      wiloc_msg_t* wilm;
+      uint8_t* macs;
+      small_size_t size;
+      small_size_t i;
+
+      PRINTF("------ sending");
+
+      dnsh = (dns_header_t*)buf;
+      dnsh->id = uint16_to_be(0xdead);
+      dnsh->flags = uint16_to_be(DNS_HDR_FLAG_RD);
+      dnsh->qdcount = uint16_to_be(1);
+      dnsh->ancount = uint16_to_be(0);
+      dnsh->nscount = uint16_to_be(0);
+      dnsh->arcount = uint16_to_be(0);
+
+      wilm = (wiloc_msg_t*)(buf + sizeof(dns_header_t));
+      wilm->vers = WILOC_MSG_VERS;
+      wilm->flags = WILOC_MSG_FLAG_TICK;
+      wilm->did = 0x2a;
+      wilm->mac_count = 16;
+
+      size = wilm->mac_count * 6;
+      macs = (uint8_t*)wilm + sizeof(wiloc_msg_t);
+      for (i = 0; i != size; ++i) macs[i] = i;
+
+      size = encode_wiloc_msg
+	((uint8_t*)wilm, SMALL_SIZEOF(wiloc_msg_t) + size);
+
+      dnsq = (dns_query_t*)(buf + sizeof(dns_header_t) + size);
+      dnsq->qtype = uint16_to_be(DNS_RR_TYPE_A);
+      dnsq->qclass = uint16_to_be(DNS_RR_CLASS_IN);
+
+      size += sizeof(dns_header_t) + sizeof(dns_query_t);
+
+      if (send_udp(buf, size, &wiloc_gwaddr, 53))
+      {
+	PERROR();
+      }
+
+      wiloc_state = WILOC_STATE_DISCONNECT;
     }
 
   case WILOC_STATE_DISCONNECT:
