36cd95efd831b255aa791760f93c08fd621e09ee
texane <texane@gmail.com>
2016-09-04 09:24:21 +0200

    device: os_linux

--- src/device/os_linux.c
+++ src/device/os_linux.c
@@ -27,7 +27,7 @@ int ICACHE_FLASH_ATTR os_udp_sendto(os_udp_t* udp, ip_addr_t* daddr, uint16_t dp
 }
 
 
-#if 0 /* TODO */
+#if 0 /* TODO, wifi_xxx */
 
 wifi_set_opmode_current(STATION_MODE);
 wifi_station_set_auto_connect(0);
@@ -43,12 +43,52 @@ wifi_get_ip_info(STATION_IF, &ipi);
 wifi_station_disconnect();
 wifi_station_dhcpc_stop();
 
-#endif /* TODO */
+#endif /* TODO, wifi_xxx */
+
+
+#if 0 /* TODO, dns_xxx */
+dns_getserver(0);
+#endif /* TODO, dns_xxx */
+
+
+/* os_timer routines */
+
+#define OS_TIMER_MAX ((unsigned long)-1)
+static unsigned long os_timer_val = 0;
+
+unsigned int ICACHE_FLASH_ATTR os_timer_is_disabled(void)
+{
+  return os_timer_val == OS_TIMER_MAX;
+}
+
+void ICACHE_FLASH_ATTR os_timer_disable(void)
+{
+  os_timer_val = OS_TIMER_MAX;
+}
+
+void ICACHE_FLASH_ATTR os_timer_rearm(unsigned long t)
+{
+  os_timer_val = t;
+}
 
 
 /* main */
 
 int main(int ac, char** av)
 {
+  while (1)
+  {
+    if (os_timer_is_disabled() == 0)
+    {
+      os_timer_val = OS_TIMER_100MS;
+      wiloc_next(NULL);
+      usleep(os_timer_val);
+    }
+    else
+    {
+      usleep(OS_TIMER_100MS);
+    }
+  }
+
   return 0;
 }
